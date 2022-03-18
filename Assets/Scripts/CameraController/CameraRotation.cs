27eb3b95e4fa0e51d4eb81ac5f9452b1fc1f3923
Мурад <52107353+Poloten4ik@users.noreply.github.com>
2022-03-18 14:32:46 +0300

    Added some backgrounds

---
+++ Assets/Scripts/CameraController/CameraRotation.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts.CameraController
+{
+    public class CameraRotation : MonoBehaviour
+    {
+        public float _speed;
+        private Vector2 _swipeDelta;
+        private float _deltaMin;
+
+        public void Update()
+        {
+
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+            {
+                _swipeDelta = Vector2.zero;
+
+                Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+                transform.Rotate(0, touchDeltaPosition.x * _speed, 0);
+            }
+        }
+    }
+}
