626cce17cd16b06134810f45ffbccf053f319ae3
Мурад <52107353+Poloten4ik@users.noreply.github.com>
2022-03-27 01:21:54 +0300

    Added Game View Scaler

---
+++ Assets/Scripts/CameraController/GameViewScaler.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts.CameraController
+{
+    public class GameViewScaler : MonoBehaviour
+    {
+
+
+
+        public void Start()
+        {
+            var resolution = (double)Screen.width / Screen.height;
+            ScaleWithoutAds(resolution);
+
+        }
+
+        private void ScaleWithoutAds(double resolution)
+
+        {
+            if (resolution < 0.49)
+            {
+                print("2280x1080");
+            }
+
+            else if (0.49 <= resolution && resolution < 0.53)
+            {
+                print("2160x1080");
+            }
+
+            else if (0.53 <= resolution && resolution < 0.58)
+            {
+                print("1920x1080");
+            }
+            else
+            {
+                print("2560x1600");
+            }
+
+        }
+    }
+}
